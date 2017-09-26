36abc2426a6558c002fcff449b4d8270b0ece823
Walter <wxfwalter@gmail.com>
2017-09-26 10:37:45 +0800

    finish list and show records

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,24 @@ class ApplicationController < ActionController::Base
   end
 
   def list_posts
-    connection = SQLite3::Database.new 'db/development.sqlite3'
-    connection.results_as_hash = true
-
     posts = connection.execute("SELECT * FROM posts")
 
-    render
+    render 'application/list_posts', locals: { posts: posts }
+  end
+
+  def show_post
+    id = params[:id]
+
+    post = connection.execute("SELECT * FROM posts WHERE id = ?", id)
+
+    render 'application/show_post', locals: {post: post.first}
+  end
+
+  private
+
+  def connection
+    db_connection = SQLite3::Database.new 'db/development.sqlite3'
+    db_connection.results_as_hash = true
+    db_connection
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get '/hello_world'       => 'application#hello_world'
   get '/hello_world/:name' => 'application#hello_world'
   get '/list_posts'        => 'application#list_posts'
+  get '/show_post/:id'     => 'application#show_post'
 end
